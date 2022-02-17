3d100ecb160aeea5555e58f574d2e39c3de5398f
Lizzy Thomson <71537606+lizzythomson@users.noreply.github.com>
2022-02-16 17:16:11 -0700

    completed queries

--- queries.sql
+++ queries.sql
@@ -6,22 +6,19 @@ SELECT product.ProductName, category.CategoryName FROM product
 LEFT JOIN category ON product.SupplierId = category.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-SELECT orderdetail.OrderID, shipper.CompanyName FROM orderdetail
-LEFT JOIN shipper ON orderdetail.ShipperID = shipper.Id
-WHERE orderdate <= '2012.08.09'
+
+SELECT "order".Id, shipper.CompanyName FROM "order"
+INNER JOIN shipper ON "order".ShipVia = shipper.Id
+WHERE orderdate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
-SELECT products.ProductName, orderdetails.Quantity FROM orderdetails
-RIGHT JOIN products ON products.ProductID = orderdetails.ProductID 
-WHERE orderdetails.OrderID = 10251 ORDER BY products.ProductName
+SELECT product.ProductName, orderdetail.Quantity FROM orderdetail
+RIGHT JOIN product ON product.Id = orderdetail.ProductId 
+WHERE orderdetail.OrderId = 10251 ORDER BY product.ProductName;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 
-
-
-
-
-select suppliername, count(*) as num_products from products as p
-join suppliers as s on s.supplierid = p.supplierid
-group by p.supplierid
+SELECT "order".Id AS order_id, customer.CompanyName AS customer_company_name, employee.LastName AS employee_last_name FROM "order"
+JOIN customer ON "order".CustomerId = customer.Id
+JOIN employee ON "order".EmployeeId = employee.Id;
